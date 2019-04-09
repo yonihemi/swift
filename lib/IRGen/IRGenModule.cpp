7a24ab97493e7360dd4bde708497d8c2c6178957
Zhuowei Zhang <zhuoweizhang@yahoo.com>
2019-04-09 15:40:01 -0700

    Add wasm object emit support

--- lib/IRGen/IRGenModule.cpp
+++ lib/IRGen/IRGenModule.cpp
@@ -1173,6 +1173,7 @@ bool IRGenModule::finalize() {
       ModuleHash->setSection("__LLVM,__swift_modhash");
       break;
     case llvm::Triple::ELF:
+    case llvm::Triple::Wasm:
       ModuleHash->setSection(".swift_modhash");
       break;
     case llvm::Triple::COFF:
