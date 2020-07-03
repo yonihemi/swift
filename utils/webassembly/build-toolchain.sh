ffdb85e0ef8b163f55e402979354937cca18bb6e
Max Desiatov <max@desiatov.com>
2020-07-03 19:37:54 +0100

    Unify per-platform host toolchain copy in scripts

--- utils/webassembly/build-toolchain.sh
+++ utils/webassembly/build-toolchain.sh
@@ -77,13 +77,9 @@ sed -i -e "s@\".*/include@\"../../../../share/wasi-sysroot/include@g" $TMP_DIR/$
 
 # Copy nightly-toolchain's host environment stdlib into toolchain
 
-if [[ "$(uname)" == "Linux" ]]; then
-  # Avoid to copy usr/lib/swift/clang because our toolchain's one is a directory
-  # but nightly's one is symbolic link, so fail to merge them.
-  rsync -a $NIGHTLY_TOOLCHAIN/usr/lib/ $TMP_DIR/$TOOLCHAIN_NAME/usr/lib/ --exclude 'swift/clang'
-else
-  cp -r $NIGHTLY_TOOLCHAIN/usr/lib/swift/macosx $TMP_DIR/$TOOLCHAIN_NAME/usr/lib/swift
-fi
+# Avoid copying usr/lib/swift/clang because our toolchain's one is a directory
+# but nightly's one is symbolic link, simple copy fails to merge them.
+rsync -a $NIGHTLY_TOOLCHAIN/usr/lib/ $TMP_DIR/$TOOLCHAIN_NAME/usr/lib/ --exclude 'swift/clang'
 
 cd $TMP_DIR
 tar cfz $PACKAGE_ARTIFACT $TOOLCHAIN_NAME
